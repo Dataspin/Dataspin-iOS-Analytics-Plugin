90c481daed3a2ef54259c9d0ef1362f4a438c446
Rafal Wilinski <rafal@dataspin.io>
2015-05-03 18:21:07 +0200

    Initial Commit

---
+++ AnalyticsSwiftSDK/AnalyticsSwiftSDK.h
@@ -0,0 +1,19 @@
+//
+//  AnalyticsSwiftSDK.h
+//  AnalyticsSwiftSDK
+//
+//  Created by Rafal on 03.05.2015.
+//  Copyright (c) 2015 Rafal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnalyticsSwiftSDK.
+FOUNDATION_EXPORT double AnalyticsSwiftSDKVersionNumber;
+
+//! Project version string for AnalyticsSwiftSDK.
+FOUNDATION_EXPORT const unsigned char AnalyticsSwiftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticsSwiftSDK/PublicHeader.h>
+
+
